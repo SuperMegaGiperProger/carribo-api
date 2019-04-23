b5dc5a63b3a91d3c8d672cf38862b89c4998e273
Pavel Pershko <thrashmetalfan98@gmail.com>
2019-04-24 02:00:02 +0300

    Add request logger

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,11 @@
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const router = require('../routes');
 
 module.exports = (app) => {
+  app.use(morgan('combined'));
+
   app.use(bodyParser.urlencoded({
     extended: true,
   }));
