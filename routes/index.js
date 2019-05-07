25a7cbc41d95f68e940cc9fc57cf03c02c0d1244
Yuliya Marhun <yuliya.marhun@gmail.com>
2019-05-07 18:11:30 +0300

    Refactor ads actions

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const checkToken = require('../middleware/checkToken');
 module.exports = (app) => {
   app.post('/v1/user_session', userSession.create);
   app.get('/v1/ads/:id', ad.read);
-  app.delete('/v1/ads/:id', checkToken, ad.delete);
+  app.delete('/v1/ads/:id', checkToken, ad.destroy);
   app.put('/v1/ads/:id', checkToken, ad.update);
   app.post('/v1/ads', ad.create);
   app.get('/v1/ads', ad.readAll);
