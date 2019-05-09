db764feb8c41eb84620a9a11997dd830abac389d
Yuliya Marhun <yuliya.marhun@gmail.com>
2019-05-09 20:24:44 +0300

    Implement getting all countries

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const ad = require('./ad');
 const photo = require('./photo');
 const adsWish = require('./adsWish');
 const user = require('./user');
+const country = require('./country');
 
 const authenticationGen = require('../middleware/authenticationGen');
 
 module.exports = (app) => {
   app.post('/v1/user_session', userSession.create);
   app.post('/v1/users', user.create);
+  app.get('/v1/countries', country.readAll);
   app.get('/v1/ads/:id', authenticationGen(false), ad.read);
   app.delete('/v1/ads/:id', authenticationGen(true), ad.destroy);
   app.put('/v1/ads/:id', authenticationGen(true), ad.update);
